28625ab9672e2101223bb71080670e6a6e18ff8f
rlagusgh0223 <hopanman11@naver.com>
2022-09-13 19:31:35 +0900

    hh

--- final_project/src/main/java/com/kosta/finalproject/repository/BoardRepository.java
+++ final_project/src/main/java/com/kosta/finalproject/repository/BoardRepository.java
@@ -1,6 +1,10 @@
 package com.kosta.finalproject.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.kosta.finalproject.model.Board;
 
@@ -9,8 +13,13 @@ import com.kosta.finalproject.model.Board;
 public interface BoardRepository  extends JpaRepository<Board, Integer> {
 
 	Board findByboardNo(Long boardNo);
+	
+//	@Modifying
+//	@Query(value = "SELECT * \r\n"
+//			+ "FROM BOARD\r\n"
+//			+ "WHERE board_status=?1", 
+//			nativeQuery = true)
+	List<Board> findByboardStatus(int status);
 
 	void deleteByboardNo(Long boardNo);
-
-	
 }
