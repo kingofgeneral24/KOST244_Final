b5e2e11fc2707306445192b29a7cec4320fb5115
koeun23 <goeun1235@gmail.com>
2022-09-15 00:39:12 +0900

    경로 진짜 마지막으로 수정

--- final_project/src/main/java/com/kosta/finalproject/model/Passenger.java
+++ final_project/src/main/java/com/kosta/finalproject/model/Passenger.java
@@ -2,13 +2,14 @@ package com.kosta.finalproject.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 @Entity(name = "PASSENGER_TABLE")
 @Data
 public class Passenger {
-	
+	@Id
 	@Column(name="BOARD_NO")
 	private Long boardNo;
 	
