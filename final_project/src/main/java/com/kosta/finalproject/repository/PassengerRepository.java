8fab3e35dc59809a0884a183a96faee1eed24360
hyepark0826 <107901144+hyepark0826@users.noreply.github.com>
2022-09-16 10:32:12 +0900

    hyepark

---
+++ final_project/src/main/java/com/kosta/finalproject/repository/PassengerRepository.java
@@ -0,0 +1,18 @@
+
+package com.kosta.finalproject.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kosta.finalproject.model.Passenger;
+
+
+public interface PassengerRepository extends JpaRepository<Passenger, Integer>{
+
+	//Passenger findByboardNo(Long boardNo);
+	List<Passenger> findByboardNo(Long boardNo);
+
+
+	
+}
