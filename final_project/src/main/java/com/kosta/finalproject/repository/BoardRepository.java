7ae5098602cadb4c40313bd01a769881e2a8ac8e
hyepark0826 <107901144+hyepark0826@users.noreply.github.com>
2022-09-17 08:49:50 +0900

    hyepark

--- final_project/src/main/java/com/kosta/finalproject/repository/BoardRepository.java
+++ final_project/src/main/java/com/kosta/finalproject/repository/BoardRepository.java
@@ -1,5 +1,9 @@
 package com.kosta.finalproject.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kosta.finalproject.model.Board;
@@ -12,5 +16,9 @@ public interface BoardRepository  extends JpaRepository<Board, Integer> {
 
 	void deleteByboardNo(Long boardNo);
 
+	List<Board> findByboardStatus(int status);
 	
+	Page<Board> findByboardTitleContaining(String searchKeyword, Pageable pageable);
+	
+	Page<Board> findByboardStatus(int status, Pageable pageable);
 }
--- final_project/src/main/java/com/kosta/finalproject/repository/PassengerRepository.java
+++ final_project/src/main/java/com/kosta/finalproject/repository/PassengerRepository.java
@@ -1,18 +1,13 @@
-
 package com.kosta.finalproject.repository;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.kosta.finalproject.model.Passenger;
-
-
 public interface PassengerRepository extends JpaRepository<Passenger, Integer>{
-
 	//Passenger findByboardNo(Long boardNo);
 	List<Passenger> findByboardNo(Long boardNo);
-
-
+	
+	Passenger findByBoardNoAndPassengerId(Long boardNo, String passengerId);
+	
+	void deleteBypassengerboardNo(Long passengerboardNo);
 	
 }
